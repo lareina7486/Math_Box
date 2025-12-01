005f8231b1b44d164e16cff0f4974f9986cefc48
lareina7486 <lareina7486@gmail.com>
2025-12-01 11:47:49 +0900

    Add comments

--- calculator.js
+++ calculator.js
@@ -1,9 +1,13 @@
+//덧셈
 const add = (a, b) => a + b;
 
+//뺄셈
 const substract = (a, b) => a - b;
 
+//곱셈
 const multiply = (a, b) => a * b;
 
+//나눗셈
 const divide = (a, b) => {
   if (b === 0) return;
   return a / b;
